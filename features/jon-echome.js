f6ea245588f906cdf1d5ffe25358984ebb1ede2d
Jon Rudy <jonr@liatrio.com>
2019-10-01 21:23:04 +0000

    Adding my new Slack Bot Feature

--- features/jon-echome.js
+++ features/jon-echome.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
 
   controller.hears('best nfl team', 'message', async(bot, message) => {
-    await bot.say("49ers are the " + message.text);
+    await bot.say("49erss are the " + message.text);
   });
 
 }
